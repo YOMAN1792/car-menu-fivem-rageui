857d6effc3ea9139a77fb7a1e3f3b59d3d701c87
YOMAN1792 <94007829+YOMAN1792@users.noreply.github.com>
2025-05-18 01:24:59 -0400

    Update cl.lua

--- lua/cl.lua
+++ lua/cl.lua
@@ -1,9 +1,4 @@
-ESX = nil
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
     TriggerServerEvent( "souki_carmenu:checkadmin")
 end)
 local statIndexboats = 1
